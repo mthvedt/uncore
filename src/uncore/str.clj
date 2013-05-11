3c6c30294456408c0dcbf9cd496190b5718646b9
Mike Thvedt <mike.thvedt@gmail.com>
2013-05-11 18:53:26 -0500

    str lib improvements

--- src/uncore/str.clj
+++ src/uncore/str.clj
@@ -1,11 +1,12 @@
 (ns uncore.str)
 
 (defn cutoff
-  ([thestr] (cutoff thestr 80))
-  ([thestr val]
-   (if (< val (count thestr))
-     (str (subs thestr 0 val) "(...)")
-     thestr)))
+  ([obj] (cutoff obj 80))
+  ([obj val]
+   (let [thestr (str obj)]
+     (if (< val (count thestr))
+       (str (subs thestr 0 val) "(...)")
+       thestr))))
 
 (defn separate-str
   [separator theseq] (apply str (drop 1 (interleave (repeat separator) theseq))))
